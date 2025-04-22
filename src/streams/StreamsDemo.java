3c0e73dec0afa9c871cc8825a5f9e79306543eec
ayushstwt <ayusrivastava2k3@gmail.com>
2025-04-23 02:26:39 +0530

    "stream.of method completed."

---
+++ src/streams/StreamsDemo.java
@@ -0,0 +1,27 @@
+package streams;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class StreamsDemo {
+    public static void main(String[] args) {
+        // Streams API is used to process a data stream.
+        //  Streams are used to process collections of objects in a functional way.
+        //  Streams are not data structures, they do not store data.
+
+        Supplier<String> supplier= ()-> "Hello streams";
+
+        Consumer<String> cunsumer= (s)-> System.out.println(s);
+
+        Stream<String> streamOfStrings = Stream.generate(supplier);
+
+        streamOfStrings.forEach(cunsumer);
+
+    }
+}
+
+
+// generate() method is used to generate an infinite stream of data.
+//  The limit() method is used to limit the number of elements in the stream.
+//  The forEach() method is used to process each element in the stream.
---
+++ src/streams/Test2.java
@@ -0,0 +1,18 @@
+package streams;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class Test2 {
+    public static void main(String[] args) {
+
+        Consumer<Integer> consumer = (i) -> System.out.println(i);
+
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        integerStream.forEach(consumer);
+
+    }
+}
+
+// The Stream.of() method is used to create a stream from a set of values.
